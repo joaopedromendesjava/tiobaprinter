0a74f293e8ab0f243f93d1b2f954702896e30461
joaopedromendesjava <joaopedromendessilva@gmail.com>
2024-05-09 20:33:27 -0300

    created project

---
+++ tiobaprinter-backend/src/main/java/com/tioba/printerbackend/TiobaprinterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tioba.printerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiobaprinterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiobaprinterBackendApplication.class, args);
+	}
+
+}
---
+++ tiobaprinter-backend/src/test/java/com/tioba/printerbackend/TiobaprinterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tioba.printerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiobaprinterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
