4348e4c6251f0ea496086ebe7627c30d5b8bd06b
joaopedromendesjava <joaopedromendessilva@gmail.com>
2024-05-11 15:33:54 -0300

    create entity toner

---
+++ tiobaprinter-backend/src/main/java/com/tioba/printerbackend/entities/Toner.java
@@ -0,0 +1,70 @@
+package com.tioba.printerbackend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Toner {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotEmpty
+	@NotNull
+	private String model;
+	
+	public Toner() {
+	
+	}
+
+	public Toner(Long id, @NotEmpty @NotNull String model) {
+		this.id = id;
+		this.model = model;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Toner other = (Toner) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
